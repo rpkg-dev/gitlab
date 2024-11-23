86027110f637eaf4944d94afe83c29eddd81d5b7
Salim B <git@salim.space>
2024-11-23 22:38:17 +0100

    chore: initial commit

---
+++ R/gitlab-package.R
@@ -0,0 +1,22 @@
+#' @section Package configuration:
+#'
+#' `r pkgsnip::md_snip(id = "pkg_config", pkg = utils::packageName())`
+#'
+#' ```{r, echo = FALSE}
+#' pal::print_pkg_config(utils::packageName())
+#' ```
+#'
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %T>%
+#' @importFrom magrittr %!>%
+#' @importFrom magrittr %$%
+#' @importFrom rlang %|%
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
---
+++ man/roxygen/meta.R
@@ -0,0 +1,5 @@
+list(
+  rd_family_title = list(common = "Other common functions:",
+                         dirs   = "Other file management functions:",
+                         files  = "Other file management functions:")
+)
